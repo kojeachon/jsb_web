93c0d9590adc01a2fa1934b7ef92e8a2d1264042
jsbchurch <jsbchurch@example.com>
2021-12-23 22:59:04 +0900

    delete

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ if os.getenv("READTHEDOCS", default=False) == "True":
     os.environ["USE_DOCKER"] = "no"
 else:
     sys.path.insert(0, os.path.abspath(".."))
+    sys.path.append(os.path.abspath("/app/<project_slug>"))
 os.environ["DATABASE_URL"] = "sqlite:///readthedocs.db"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
